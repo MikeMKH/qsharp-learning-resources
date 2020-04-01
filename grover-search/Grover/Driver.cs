56dae5ecb7ac3761694641440bb683256ccfb918
Mike Harris <MikeMKH@users.noreply.github.com>
2020-04-01 06:29:04 -0500

    init

---
+++ grover-search/Grover/Driver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Microsoft.Quantum.Samples.SimpleGrover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sim = new QuantumSimulator();
+            var nDatabaseQubits = 5;
+
+            var result = SearchForMarkedInput.Run(sim, nDatabaseQubits).Result;
+            Console.WriteLine($"Result: {result}");
+
+            Console.WriteLine("\n\nPress any key to continue...\n\n");
+            Console.ReadKey();
+        }
+    }
+}
--- grover-search/README.md
+++ grover-search/README.md
@@ -1,1 +1,2 @@
-# Grover Search Algorithm
+# Grover's Search Algorithm
+https://docs.microsoft.com/en-us/quantum/quickstarts/search?tabs=tabid-csharp
